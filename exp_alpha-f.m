100bb4e92088fe7460ba4d002f3ebca403d378cc
Emil Maagaard <emaagaard@gmail.com>
2016-11-10 14:58:15 +0100

    Add subfolders for data

--- exp_alpha-f.m
+++ exp_alpha-f.m
@@ -9,41 +9,38 @@ model.alpha = 2; % 2?
 model.dimension = 1;
 
 
-iterations = 200;
-intraIterations = 400;
-numFuncs = 10; 
+iterations = 500;
+intraIterations = 20;
 
 logLikelihood = zeros(iterations, intraIterations);
+
 ratios = zeros(iterations, intraIterations);
+
 alphas = zeros(iterations, intraIterations);
 betas = zeros(iterations, intraIterations);
-estimatedWeights = zeros(iterations, intraIterations, numFuncs);
-trueWeights = zeros(iterations, intraIterations, numFuncs);
+%weights = zeros(iterations, intraIterations);
 
-dataTile = datetime('now');
-data.numSamples = 'iter*25';
-
-for iter = 1:iterations
-    
-    for intraIter = 1:intraIterations
-        
-        numSamples = iter*25;
+for iter=1:iterations
+    for intraIter=1:intraIterations 
+        numSamples = 100;
         
         functions = {};
-
+        numFuncs = iter; %iter;
         limit = numFuncs/2;
         
         for i=1:numFuncs
-            mu_j=-limit+i/2;
+            mu_j=-limit+i-0.5;
             s = 1;      % spatial scale
             functions{i} = @(x) exp(-((x-mu_j).^2)/(2*s^2));
         end
         
+        % Should this be multi variate ?!?!?!?!?
+        % Sampling
+        
         % Random values of w
-        model.w = normrnd(0,sqrt(1/model.alpha), [1 length(functions)]);  %*eye(model.dimension)
-        trueWeights(iter, intraIter,:) = model.w';
-
-        trainX = unifrnd(-2,2, [model.dimension numSamples]);
+        model.w = normrnd(0,sqrt(1/model.alpha), [1 length(functions)+1]);  %*eye(model.dimension)
+        
+        trainX = unifrnd(-3,3, [model.dimension numSamples]);
         targets= zeros(model.dimension, length(trainX));
         targetNoise = zeros(model.dimension, length(trainX));
         
@@ -53,6 +50,7 @@ for iter = 1:iterations
             targetNoise(i) = normrnd(model.noiseMean, model.sigma);
             targets(i) = trainY(i) +  targetNoise(i);
         end
+        % plot(trainX, targets, 'm+');
         
         %%% Bayesian inference
         Phi = PhiMatrix(functions, trainX);
@@ -60,57 +58,45 @@ for iter = 1:iterations
         beta_init = rand;
         [alpha, beta, w, llh] = maximum_evidence(alpha_init, beta_init, Phi, targets');
         
+%         [alpha, beta, w, llh] = bayes(functions, trainX, targets);
+        
         betas(iter, intraIter) = beta;
         alphas(iter, intraIter) = alpha;
         
         ratios(iter, intraIter) = alpha/beta;
 
-        estimatedWeights(iter, intraIter, :) = w;
-        
+%         wDiffer(iter,:) = wDiffer(iter,:) + abs(w'-model.w);
         logLikelihood(iter, intraIter) = llh;
-        
+        %weights(iter,intraIter) = w';
+
         if mod(intraIter,50) == 0
             intraIter
         end
     end
     
-    if mod(iter,10) == 0
-        iter
-    end
-    
-    if mod(iter, 50) == 0
-        % Save data in case of being stopped early
-        data.currentIteration = iter;
-        data.currentIntraIteration = intraIter;
-        data.iterations = iterations;
-        data.intraIterations = intraIterations;
-        data.numFuncs = numFuncs;
-        data.ratios = ratios;
-        data.model = model;
-        data.alphas = alphas;
-        data.betas = betas;
-        data.llh = logLikelihood;
-        data.wTrue = trueWeights;
-        data.wEstimated = estimatedWeights;
-        
-        save(['exp_w-diff/' datestr(dataTile)], 'data');
-    end
-    
+    iter
 end
 
+ratio_means = mean(ratios,2);
+
+trueRatio = (model.alpha/model.beta);
+
+estimatedRatio = mean(ratios,2);
 
 %% Prepare data for saving
-data.currentIteration = iter;
-data.currentIntraIteration = intraIter;
 data.iterations = iterations;
 data.intraIterations = intraIterations;
 data.numFuncs = numFuncs;
 data.ratios = ratios;
+
 data.model = model;
 data.alphas = alphas;
+
 data.betas = betas;
 data.llh = logLikelihood;
-data.wTrue = trueWeights;
-data.wEstimated = estimatedWeights;
+%data.w = weights;
 
-save(['exp_w-diff/' datestr(dataTile)], 'data');
+data.numSamples = '100';
+
+save(['exp_alpha-f/' datestr(datetime('now'))], 'data');
+
