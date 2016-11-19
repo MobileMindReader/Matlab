fe6fdefe3fc965a6d04a4b5ba828fef671dd7c69
Emil Maagaard <emaagaard@gmail.com>
2016-11-19 17:08:14 +0100

    Add evidence to rocenstruction

--- exp_forward_problem.m
+++ exp_forward_problem.m
@@ -12,7 +12,7 @@ reducedModel = forwardModel(:,1:reducedSize);
 % RandStream.setGlobalStream(s);
 
 %%
-timeSteps=100;
+timeSteps=1;
 A=reducedModel;
 
 x=zeros(reducedSize, timeSteps);
@@ -24,7 +24,7 @@ for i=activeIndexes
     offset=randn;
     scale=2;
     for t=1:timeSteps
-        x(i,t) = offset + scale * sin(t);
+        x(i,t) = scale * sin(t+offset*pi);
     end
 end
 [rows, ~] = find(x);
@@ -33,33 +33,75 @@ clearvars 'offset' 'scale' 'i' 't' 'rows'
 
 %% Construct sensor measurement
 
-y = A * x + sqrt(1/0.01)*randn(size(A,1),timeSteps);
+y = A * x;% + sqrt(1/0.01)*randn(size(A,1),timeSteps);
 
 % plot(1:timeSteps, y)
 % surf(y)
+
+
+
+%% Determine sparsity 
+
+alpha_init = eye(size(A,2));
+% alpha_init(find(alpha_init)) = rand(1,size(A,2));
+alpha_init(logical(eye(size(alpha_init)))) = rand(1,size(A,2));
+beta_init = rand;
+
+[Q, beta, mn, llh] = maximum_evidence_multi(alpha_init, beta_init, A, y);
+
+estimatedIndexes=find(diag(Q) ~=10000);
+
+
+
+
+
+
+
+
+
+
+
 %% Reconstrucion
-alpha = 10.0;
-beta = 2;
+% alpha = 10.0;
+% beta = 2;
 
-Q = 1/1e6 * eye(size(A,2));
-
-Q(activeIndexes,activeIndexes) = 1/alpha;
+% Q = 1/1e6 * eye(size(A,2));
+% 
+% for i=activeIndexes
+%     Q(i,i) = 1/(alpha);
+% end
 
 x_est=((Q * A') /(1/beta*eye(size(A,1)) + A*Q*A'))*y;
+
+figure(100)
+subplot(2,1,1), plot(x);
+subplot(2,1,2), plot(x_est);
+%%
 
 figure(1)
 surf(x), view(90,0);
 figure(2)
-surf(x_est), view(90,0);
+% surf(x_est), view(90,0);
 
 
+%%
 figure(3)
-idx=activeIndexes(6);
-subplot(2,1,1), plot(1:timeSteps,x_est(idx,:));
-title('Source reconstructed signal');
-subplot(2,1,2), plot(1:timeSteps,x(idx,:));
-title('Simulated source signal');
 
+
+for i=1:4
+    idx=activeIndexes(i);
+    subplot(4,4,i), plot(1:timeSteps,x_est(idx,:));
+    title('Source reconstructed signal');
+    subplot(4,4,i+4), plot(1:timeSteps,x(idx,:));
+    title('Simulated source signal');
+end
+for i=5:8
+    idx=activeIndexes(i);
+    subplot(4,4,4+i), plot(1:timeSteps,x_est(idx,:));
+    title('Source reconstructed signal');
+    subplot(4,4,8+i), plot(1:timeSteps,x(idx,:));
+    title('Simulated source signal');
+end
 %%
 x_est = y'/(A');
 % x_est_alt= (A')/(y');
